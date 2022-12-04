ce6bf0325523e9143eb7fcdf2236fa7db615c789
Ujjwal <ujjwaljha1225@gmail.com>
2022-12-04 06:16:58 +0545

    Here the function call occured before defining it so it threw an err. To avoid this the function was definied earlier before calling it.

--- second.c
+++ second.c
@@ -2,17 +2,6 @@
 #define MAX 4
 int stack_arr[MAX];
 int top = -1;
-
-int main()
-{
-    push(1);
-    push(2);
-    push(3);
-    push(4);
-    push(5);
-    return 0;
-}
-
 void push(int data)
 {
     top += 1;
@@ -22,4 +11,14 @@ void push(int data)
         return;
     }
     stack_arr[top] = data;
+}
+
+int main()
+{
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    push(5);
+    return 0;
 }
