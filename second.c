a2c0f0910ef8c9108e8e9b99d0dcfa3e2f2d2cd8
Ujjwal <ujjwaljha1225@gmail.com>
2022-12-04 06:02:24 +0545

    Stack Overflow

---
+++ second.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define MAX 4
+int stack_arr[MAX];
+int top = -1;
+
+int main()
+{
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    push(5);
+    return 0;
+}
+
+void push(int data)
+{
+    top += 1;
+    if (top == MAX)
+    {
+        printf("Stack Overflow!");
+        return;
+    }
+    stack_arr[top] = data;
+}
