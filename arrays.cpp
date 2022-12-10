769bf683343adc449f2d54f9849856a6f47fdad3
Ujjwal <ujjwaljha1225@gmail.com>
2022-12-10 10:37:21 +0545

    Selection sort Learnt and Used

--- arrays.cpp
+++ arrays.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <array>
+using namespace std;
 // Array is a collection of variables of same data type
 int arr[50];
 // Given statement simply means aauta array declare bhayo having 50 integer elements
@@ -6,13 +9,44 @@ int arr[50];
 // It is called lists in python and array list in Java
 // The size of vectors can increase as well as decrease
 
-// An array [5, 7, 8, 13, 1, 6, 20, 21] is given and now we are supposed to find a pair (a,b) such that their sum if k. Here lets suppose 25
-// Two summation problem
-#include <iostream>
-#include <array>
-using namespace std;
+
+// Printing an Array 
+void PrintArray(int *A, int n)
+{
+    for (int i=0; i<n; i++)
+    {
+        printf("%d\t", A[i]);
+    }
+    printf("\n");
+}
+
+// SELECTION SORT
+// Basically what we do is select one element as min and then start compairing it with the rest of elements of array
+// Then we get the minimum element and place it in the rqrd position
+void selection_sort(int *A, int n)
+{
+    printf("Running Selection Sort...\n");
+    int index_of_min;
+    for (int i=0; i<n; i++)
+    {
+        index_of_min = i;
+        for (int j=i; j<n; j++)
+        {
+            if(A[j]<A[index_of_min])
+            {
+                index_of_min = j;
+            }
+        }
+        int temp = A[i];
+        A[i] = A[index_of_min];
+        A[index_of_min] = temp;
+    }
+}
+
 int main()
 {
+// An array [5, 7, 8, 13, 1, 6, 20, 21] is given and now we are supposed to find a pair (a,b) such that their sum if k. Here lets suppose 25
+// Two summation problem
     int arr[] = {5, 7, 8, 13, 1, 6, 20, 21};
     // See how to check the data type of Array >> I heard of is_array function but not sure how to use it
     int n;
@@ -33,7 +67,21 @@ int main()
         }
     }
     // Hence we can easily get the ordered pairs from array which is able to give definite solution to double summation problem but the time complexity becomes n^2 so its not very efficient as a solution
-    
 
+
+    // Again the time complexity can be reduced using hashmap but the space complexity increases 
+
+    // The best and optimal solution to this question would be two pointer method where we use start and end pointers and check if the sum is less, equal or greater than target
+    //If the solution doesnt exist then s and e would coincide.
+
+    // Creating new array to be sorted by selection sort
+    int array1[] = {8, 0, 7, 1, 6};
+    n = sizeof(array1) / sizeof(array1[0]);
+    // Calling print function to print our array
+    PrintArray(array1, n);
+    // Calling sort function
+    selection_sort(array1, n);
+    // Printing after sorting
+    PrintArray(array1, n);
     return 0;
 }
