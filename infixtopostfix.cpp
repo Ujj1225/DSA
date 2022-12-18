6dac0913cc25a0df6b05e5d227bb0ffbad6080b1
Ujjwal <ujjwaljha1225@gmail.com>
2022-12-18 19:58:47 +0545

    Making function of precedence to make conversion easier

--- infixtopostfix.cpp
+++ infixtopostfix.cpp
@@ -1,15 +1,40 @@
 #include<iostream>
 #include<stack>
-#include<string>
-#define Max 10
-
-char stack[Max];
-// This stack arr stores all characters even left and right parenthesis
-char infix[Max], postfix[Max];
+using namespace std;
 int top =-1;
-void inToPost()
+// Function which returns the precedence by scanning the symbols
+int prec(char c)
 {
-    
+    if (c=='^')
+        return 3;
+    else if (c == '*' || '/')
+        return 2;
+    else if (c == '+' || '-')
+        return 1;
+    else
+        return -1;
+}
+
+string infixtopostfix(string s){
+    stack<char> st;
+    string res;
+    for(int i =0; i< s.length; i++)
+    {
+        if((s[i]>='a' && s[i]<='z') || (s[i]>= 'A' && s[i]<= 'Z'))
+        {
+            res += s[i];
+        }
+        else if (s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            while(!st.empty )
+        }
+    }
+
+
 
 
 }
